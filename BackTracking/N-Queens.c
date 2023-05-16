c53030c9311897408e43e0df9211367d9fce107b
ShaunFernandes22 <111562711+ShaunFernandes22@users.noreply.github.com>
2023-05-16 08:02:34 +0530

    Update N-Queens.c

--- BackTracking/N-Queens.c
+++ BackTracking/N-Queens.c
@@ -6,12 +6,15 @@ soln[kth_row_Queen] = col_no*/
 #include <stdlib.h>
 #include <stdbool.h>
 
+int solution[30]; //soln array of col_no
+int n;
+
 // bounding function
-bool inPlace(int solution[], int k, int j){
+bool inPlace(int k, int j){
     int i;
     for (i=0; i<k; i++) // checking all rows that have been filled before k
     {
-        // i-> row, j ->particular column
+        // i-> row, j ->specific column checked
         //   same column         same diagonal
         if ((solution[i]==j) || abs(solution[i] - j) == abs(k-i))
             return false;
@@ -19,12 +22,12 @@ bool inPlace(int solution[], int k, int j){
     return true;
 }
 
-void nQueens(int solution[], int k, int n){
+void nQueens(int k){
     int i;
     static int count=0; //no_of solns
     for (i=0; i<n; i++) // checking all possible columns
     {
-        if (inPlace(solution, k, i)) // checks if the position (k, i) is valid
+        if (inPlace(k, i)) // checks if the position (k, i) is valid
         {
             solution[k] = i; // add i as column_no of kth Queen
             if (k==n-1) // all rows filled
@@ -32,11 +35,11 @@ void nQueens(int solution[], int k, int n){
                 count++;
                 printf("Solution %d\n", count);
                 for (int ct =0; ct <n; ct++)
-                    printf("Queen %d placed in %d column\n",ct,solution[ct]);
+                    printf("Queen %d placed in column %d\n",ct+1,solution[ct]+1);
                 printf("\n");    
             }
             else
-                nQueens(solution, k+1, n); //finding col for next Queen
+                nQueens(k+1); //finding col for next Queen
 
             // printf("Dead end, Recursive call comes back to Queen %d\n",k) ;
             // checking more possible locations for kth queen as failure in children (backtracking) or success.
@@ -45,11 +48,10 @@ void nQueens(int solution[], int k, int n){
 }
 
 int main() {
-    int n;
     printf("Enter the number of Queens on n x n board (value of n) : ");
     scanf("%d",&n);
-    int solution[30]; //soln array of col_no
+    
     int k=0;
     //we call for the 0th queen, rest is called recursively
-    nQueens(solution, k, n);
+    nQueens(k);
 }
