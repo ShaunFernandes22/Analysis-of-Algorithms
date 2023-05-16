47388f9567e2176297bb77d6b77b406d90d0ae22
ShaunFernandes22 <111562711+ShaunFernandes22@users.noreply.github.com>
2023-05-16 12:04:38 +0530

    removed unnecessary iterations

--- DivideAndConquer/Insertion&Selection.c
+++ DivideAndConquer/Insertion&Selection.c
@@ -19,10 +19,10 @@ void insertionSort(int arr[], int n)
 }
 
 void selectionSort(int a[], int n){
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n-1; i++)
     {
         int min =i;
-        for(int j=1;j<n;j++)
+        for(int j=i+1;j<n;j++)
         {
             if (a[j] > a[i])
             min = j;
@@ -39,24 +39,53 @@ void selectionSort(int a[], int n){
 int main() {
     clock_t start_t, end_t;
     double total_t;
-    int i, n = 10000;
-    int arr[10];
+    int i, n = 15000;
+    int arr[n];
+    
     for(i=0; i<n; i++)
-    {
-       //arr[i] = rand() ;//%15000; //-->average case
-        //arr[i] = i+1; //-->best case
-        arr[i] = 15000 -i; //--> worst case
-    }
+        arr[i] = i+1; //-->best case
+    
     start_t = clock();
     printf("Starting of the program, start_t = %ld\n", start_t);
     
-    //insertionSort(arr, n);
-    //selectionSort(arr, n);
+    insertionSort(arr, n);
+    // selectionSort(arr, n);
  
    end_t = clock();
-   printf("End of the big loop, end_t = %ld\n", end_t);
+   printf("End of the sorting, end_t = %ld\n", end_t);
+   
+   total_t = (double)(end_t - start_t); 
+   printf("Total time taken by CPU - Best Case : %f\n",total_t);
+   
+   
+   for(i=0; i<n; i++)
+       arr[i] = rand()%n ;//-->average case
+
+    start_t = clock();
+    printf("\nStarting of the program, start_t = %ld\n", start_t);
+    
+    insertionSort(arr, n);
+    // selectionSort(arr, n);
+ 
+   end_t = clock();
+   printf("End of the sorting, end_t = %ld\n", end_t);
+   
+   total_t = (double)(end_t - start_t); 
+   printf("Total time taken by CPU - Average Case : %f\n",total_t);
+   
+   
+   for(i=0; i<n; i++)
+        arr[i] = n - i; //--> worst case
+    start_t = clock();
+    printf("\nStarting of the program, start_t = %ld\n", start_t);
+    
+    insertionSort(arr, n);
+    // selectionSort(arr, n);
+ 
+   end_t = clock();
+   printf("End of the sorting, end_t = %ld\n", end_t);
    
    total_t = (double)(end_t - start_t); 
    printf("Total time taken by CPU: %f\n",total_t);
-   printf("Exiting of the program...\n");
+ 
 }
