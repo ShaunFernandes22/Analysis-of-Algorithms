a450160a70e850d469b42457725d5c42cd6b68b5
ShaunFernandes22 <111562711+ShaunFernandes22@users.noreply.github.com>
2023-05-18 23:43:50 +0530

    Update SumOfSubset.c

--- BackTracking/SumOfSubset.c
+++ BackTracking/SumOfSubset.c
@@ -3,20 +3,23 @@ s + r - w[k] >= m, s + w[k+1] <= m --> sum possible without w[k]*/
 
 #include <stdio.h>
 
-void printSolution(int x[], int w[], int n)
+int w[20], x[20];
+int n, m;
+
+void printSolution()
 {
     for (int i=0; i<n; i++)
         printf("%d \t", x[i]);
     printf("\n");    
-    printf("[ ");
+    printf("{ ");
     for (int i=0; i<n; i++)
         if (x[i] == 1)
             printf("%d ", w[i]);
-    printf("]");        
+    printf("}");        
     printf("\n");    
 }
 
-void sumOfSubset(int w[], int x[], int s, int k, int r, int m, int n)
+void sumOfSubset(int s, int k, int r)
 {
     static int count = 0;
     x[k] = 1;
@@ -28,23 +31,23 @@ void sumOfSubset(int w[], int x[], int s, int k, int r, int m, int n)
         // resetting 1's after k which were part of previous solutions, since we're back to k
         for (int ct=k+1; ct<n; ct++)
             x[ct] = 0;
-        printSolution(x, w, n);
+        printSolution();
     }
     // calling left subtree (dfs) going 1 level deeper
     else if (s + w[k] + w[k+1] <= m)
-        sumOfSubset(w, x, s + w[k] , k+1, r - w[k], m, n);
+        sumOfSubset(s + w[k] , k+1, r - w[k]);
 
     // calling right subtree after coming coming back to parent call (backtracking) on either success or dead ends -> not possible to get sum further 
     if ((s + r - w[k] >= m) && (s + w[k+1] <= m)) // checking if sum is possible in right tree
     {
         x[k] = 0;
-        sumOfSubset(w, x, s, k+1, r - w[k], m, n); //dfs
+        sumOfSubset(s, k+1, r - w[k]); //dfs
     }
 }
 
 int main() {
-    int i, k=0, sum=0, rem=0, n, m;
-    int w[20], x[20];
+    int i, k=0, sum=0, rem=0;
+    
     printf("Enter the number of weights : ");
     scanf("%d",&n);
     for (i=0; i<n; i++)
@@ -57,10 +60,10 @@ int main() {
         rem += w[i];
         x[i] = 0;
     }   
-    // printSolution(solution, n);
+
     printf("\nEnter the sum needed : ");
     scanf("%d",&m);
-    sumOfSubset(w, x, sum, k, rem, m, n);    
+    sumOfSubset(sum, k, rem);    
 
     return 0;
 }
